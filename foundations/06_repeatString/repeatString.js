05c23c90d4b41527d1275c798c0001f50b5351d3
tristanvangarsse <159120739+tristanvangarsse@users.noreply.github.com>
2026-04-15 08:57:16 +0200

    Finished foundation exercises 1-6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num >= 0) {
+        let combined = "";
+        for (let i = 0; i < num; i++) {
+            combined += string;
+        }
+        return combined;
+    } else {
+        return "ERROR";
+    }
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
